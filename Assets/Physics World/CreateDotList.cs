c2b9577dba10a18692504f2b6744459d1b3e6733
ubceml <32344746+ubceml@users.noreply.github.com>
2017-11-08 12:50:51 -0800

    Augmented Project

---
+++ Assets/Physics World/CreateDotList.cs
@@ -0,0 +1,83 @@
+﻿namespace Vuforia {
+    using UnityEngine;
+    using System.Collections.Generic;
+
+    public class CreateDotList : MonoBehaviour {
+        public GameObject dott;
+        public float arDist;
+
+        public Material newMaterial;
+        public Material oldMaterial;
+        public LineRenderer trajectory;
+
+        //for calculating diffference
+        public DrawTrajectory spacedot;
+        private int count = 0;
+        private int dotsOnTrajectory;
+        private List<GameObject> dotList = new List<GameObject>();
+
+        public void Update() {
+            dotsOnTrajectory = spacedot.dot.Count;  // in Update because dots would not draw if declared in Start()
+
+            checkScreenTouched();
+            resetScreen();
+        }
+
+        void checkScreenTouched() {
+            foreach (Touch touch in Input.touches) {
+                if (touch.phase == TouchPhase.Began && count < dotsOnTrajectory) {
+                    createTouchDot(touch.position);
+                }
+            }
+        }
+
+        void resetScreen() {
+
+            if (Input.GetKeyDown(KeyCode.Escape)) {     // if the back button on an Android phone is touched, reset the trajctory
+                foreach (GameObject createdDot in dotList)
+                    Destroy(createdDot);
+
+                dotList.Clear();
+                trajectory.material = oldMaterial;
+
+                for (int i = 0; i < count; i++) {
+                    spacedot.dot[i].GetComponent<Renderer>().material = oldMaterial;
+                }
+                count = 0;
+            }
+        }
+
+        void createTouchDot(Vector3 position) // if Vive trigger is pressed
+        {
+            Vector3 touchPos = position;                      
+            float relativeDist =  Camera.main.transform.position.z - arDist;
+            touchPos.z = Mathf.Abs(relativeDist);                                             // distance from camera of ImageTarget
+
+            Vector3 placeDot = Camera.main.ScreenToWorldPoint(touchPos);
+
+            dotList.Add(Instantiate(dott, placeDot, transform.rotation));  // keep z-position at 0 so that the dots stay along the trajectory line
+            displayDots(touchPos, count);
+            count++;
+            displayTrajectory();
+        }
+
+        void displayDots(Vector3 position, int index) {
+            float dist = 0;
+
+
+            if (spacedot.dot.Count > 0 && index < spacedot.dot.Count) {
+                GameObject closestDot = spacedot.dot[index];
+                dist = Vector3.Distance(closestDot.transform.position, position); // calculate the distance from the trajectory to the input
+                Debug.Log(dist);
+                //mText.text = "Error: " + dist;
+                closestDot.GetComponent<Renderer>().material = newMaterial; //changes the material from transparent to green
+                
+            }
+        }
+
+        void displayTrajectory() {
+            if (count >= spacedot.dot.Count)
+                trajectory.material = newMaterial; //changes the line of the trajectory to be visible
+        }
+    }
+}
---
+++ Assets/Physics World/DrawTrajectory.cs
@@ -0,0 +1,108 @@
+﻿namespace Vuforia {
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class DrawTrajectory : MonoBehaviour {
+        public GameObject parentObject;
+        public Material transparent;
+
+        //public
+        public float speed = 18.0f; // the initial speed
+        public float angle = 45.0f;// give an initial angle
+
+        private Vector3 start; // start points
+        private Vector3 end; // end points
+
+        private float angle_r = 0.0f;// angle_r in radiant
+        private float timeResolution = 0.02f;
+        private float maxTime = 0.0f;
+        public GameObject startPoint;
+        public List<GameObject> dot = new List<GameObject>();
+
+        public GameObject dott; //creating dots
+        public int addDot = 0;
+
+        //private
+        private Vector3 Velocity_Vector;
+        private Vector3 scaleVector = new Vector3(0.05f, 0.05f, 0.05f);     // found through testing and observation (can be changed)
+
+        //private Sphere sphere;
+        private float Velocity_Y = 0.0f;
+        private float Velocity_X = 0.0f;
+        private int index = 0;
+        private float gravity = Physics.gravity.magnitude;
+
+        //draw path variables
+        private List<Vector3> positions = new List<Vector3>();
+        private LineRenderer trajectory;
+        public GameObject path;
+
+        // determine whether to start drawing dots
+        public bool startDrawing;
+
+        // Use this for initialization
+        void Start() {
+            start = startPoint.transform.position;
+            //first get the radiant
+            angle_r = (angle / 180.0f) * Mathf.PI;
+            Velocity_X = speed * Mathf.Cos(angle_r);
+            Velocity_Y = speed * Mathf.Sin(angle_r);
+
+            maxTime = (2.0f * Velocity_Y) / 9.81f;
+            startPoint.transform.rotation = Quaternion.Euler(angle, 0.0f, 0.0f);
+            startDrawing = true;
+        }
+
+        void Update() {
+            if (startDrawing)
+                drawDots(); // checks if startDrawing is true
+        }
+
+        void drawDots() {
+            //int maxIndex = 9 * (int)(maxTime / timeResolution);    // draw dots from 0-9 (10 in total)
+                                                                   //Debug.Log (maxIndex);
+            timeResolution += 0.25f;
+
+            float dx = speed * timeResolution * Mathf.Cos(angle * Mathf.Deg2Rad);
+            float dy = speed * timeResolution * Mathf.Sin(angle * Mathf.Deg2Rad) - (gravity * timeResolution * timeResolution / 2.0f);
+
+            Vector3 currentPosition = new Vector3(start.x + dx, start.y + dy, 0); //change from 0 to 2
+            positions.Add(currentPosition);
+
+
+            if (positions[index].y <= start.y && index > 1) {       //makes sure that the points are drawn till the position is <= the starting y-position
+                drawTrajectory();
+                return;
+            }
+
+            if (addDot % 2 == 0) {
+                GameObject newDot = Instantiate(dott, currentPosition, transform.rotation); // draw all dots that are divisble by 2
+
+                newDot.transform.parent = parentObject.transform;
+                newDot.transform.localScale = scaleVector; // resizes createdDot to be the same size as startPoint
+
+                dot.Add(newDot);
+            }
+
+            addDot++;
+            index++;
+            Debug.Log(index);
+        }
+
+        void drawTrajectory() {
+            int count = 0;
+            trajectory = path.GetComponent<LineRenderer>();
+
+            Vector3[] coordinates = new Vector3[positions.Count];
+            trajectory.positionCount = index;
+            positions.CopyTo(coordinates);
+
+            while (count < index) {
+                trajectory.SetPosition(count, coordinates[count]);
+                count++;
+            }
+
+            startDrawing = false; 
+        }
+    }
+}
